781a5d7944763dcbfdd421e77ed293622d0130ed
ibrahimcheddad2021-afk <ibrahimcheddad2021@gmail.com>
2025-12-03 22:01:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("connect-btn");
+
+  btn.addEventListener("click", () => {
+    alert("Rithmic API integration will be added soon!");
+  });
+});
