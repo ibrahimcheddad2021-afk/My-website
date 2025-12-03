9648e49a9f18877349c2f9100f6a66fb3f910a4e
ibrahimcheddad2021-afk <ibrahimcheddad2021@gmail.com>
2025-12-03 23:35:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("connect-btn");
-
-  btn.addEventListener("click", () => {
-    alert("Rithmic API integration will be added soon!");
-  });
-});
